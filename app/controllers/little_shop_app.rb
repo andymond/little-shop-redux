d8d989780458dd7d3baa17cb82b737062dffbb13
heybluguy <heybluguy@yahoo.com>
2018-01-24 16:52:29 -0700

    Merchants can be created

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -5,4 +5,19 @@ class LittleShopApp < Sinatra::Base
     erb :"merchants/index"
   end
 
+
+
+
+
+  get '/merchants/new' do
+    erb :"merchants/new"
+  end
+
+  post '/merchants' do
+    merchant = Merchant.new(params[:merchant])
+    merchant.save
+    redirect '/merchants'
+  end
+
+
 end
