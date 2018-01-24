6037628c27548389706d7bd0377a0681241bb120
heybluguy <heybluguy@yahoo.com>
2018-01-23 18:44:58 -0700

    Merchant model created merchant spec passing

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,9 +1,35 @@
 describe Merchant do
   describe "Validations" do
-    it "is invalid without a name" do
-      merchant = Merchant.create()
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        merchant = Merchant.create()
 
-      expect(merchant).to_not be_valid
+        expect(merchant).to_not be_valid
+      end
+
+      it "is invalid if its not unique" do
+        merchant_1 = Merchant.create(name: "Pepsi")
+        merchant_2 = Merchant.create(name: "Pepsi")
+
+        expect(merchant_2).to_not be_valid
+        expect(merchant_1).to be_valid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a name" do
+        merchant = Merchant.create(name: "Pepsi")
+
+        expect(merchant).to be_valid
+      end
+
+      it "is valid if its unique" do
+        merchant_1 = Merchant.create(name: "Pepsi")
+        merchant_2 = Merchant.create(name: "Coke")
+
+        expect(merchant_2).to be_valid
+        expect(merchant_1).to be_valid
+      end
     end
   end
 end
