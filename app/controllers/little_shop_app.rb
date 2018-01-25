df197b94708738ab8d529cbeac7ba3ec4fd60554
Andrew Dymond <andy.dymond@gmail.com>
2018-01-24 17:11:16 -0700

    Complete edit merchant action in controller & edit merchant view template

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -4,10 +4,20 @@ class LittleShopApp < Sinatra::Base
     @merchants = Merchant.all
     erb :"merchants/index"
   end
-  
+
   get '/merchants/:id' do
     @merchant = Merchant.find(params[:id])
     erb :"merchants/show"
+  end
+
+  get '/merchants/:id/edit' do
+    @merchant = Merchant.find(params[:id])
+    erb :"merchants/edit"
+  end
+
+  put '/merchants/:id' do |id|
+    Merchant.update(id.to_i, params[:merchant])
+    redirect "/merchants/#{id}"
   end
 
   delete '/merchants/:id' do
