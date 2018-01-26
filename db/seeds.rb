d962a6f84590bd6d72879da59610442cd8753097
Joe Jobes <jmrjobes@gmail.com>
2018-01-25 19:34:10 -0700

    Create initial seed structure for items

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'csv'
 require './app/models/merchant.rb'
 
 Merchant.destroy_all
-Item.destroy_all
+# Item.destroy_all
 
 merchants = CSV.open './data/merchants.csv', headers: true, header_converters: :symbol
 merchants.each do |row|
@@ -12,14 +12,12 @@ merchants.each do |row|
                   created_at: row[:created_at])
 end
 
-items = CSV.open './data/items.csv', headers: true, header_converters: :symbol
-items.each do |row|
-Item.create(id: row[:id],
-            name: row[:name],
-            description: row[:description],
-            price: row[:unit_price],
-            updated_at: row[:updated_at],
-            created_at: row[:created_at])
-end
-
- items = CSV.open './data/item_images.csv'
+# items = CSV.open './data/items.csv', headers: true, header_converters: :symbol
+# items.each do |row|
+# Item.create(id: row[:id],
+#             name: row[:name],
+#             description: row[:description],
+#             price: row[:unit_price],
+#             updated_at: row[:updated_at],
+#             created_at: row[:created_at])
+# end
