62b89735c30ace51b3e8e200a2ea1dcb72bc1348
Joe Jobes <jmrjobes@gmail.com>
2018-01-26 16:44:10 -0700

    Implement Items seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 require 'csv'
 require './app/models/merchant.rb'
+require './app/models/item.rb'
 
 Merchant.destroy_all
-# Item.destroy_all
+Item.destroy_all
 
 merchants = CSV.open './data/merchants.csv', headers: true, header_converters: :symbol
 merchants.each do |row|
@@ -12,14 +13,14 @@ merchants.each do |row|
                   created_at: row[:created_at])
 end
 
-# items = CSV.open './data/items.csv', headers: true, header_converters: :symbol
-# items.each do |row|
-# Item.create(id: row[:id],
-#             name: row[:name],
-#             description: row[:description],
-#             price: row[:unit_price],
-#             updated_at: row[:updated_at],
-#             created_at: row[:created_at],
-#             merchant_id: row[:merchant_id],
-#             image: 'https://openclipart.org/image/2400px/svg_to_png/227732/Potion.png')
-# end
+items = CSV.open './data/items.csv', headers: true, header_converters: :symbol
+items.each do |row|
+Item.create(id: row[:id],
+            title: row[:name],
+            description: row[:description],
+            price: row[:unit_price],
+            updated_at: row[:updated_at],
+            created_at: row[:created_at],
+            merchant_id: row[:merchant_id],
+            image: 'https://openclipart.org/image/2400px/svg_to_png/227732/Potion.png')
+end
