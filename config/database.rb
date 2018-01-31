558d8ac3daf538b1001b0d76bc20b9c9534d73ee
Andrew Dymond <andy.dymond@gmail.com>
2018-01-31 10:41:49 -0700

    update config

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,6 @@
 # set the database based on the current environment
 database_name = "little-shop-#{LittleShopApp.environment}"
-db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
+db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}") || "postgres://bxtxihnjojbwaw:d3df0c094b41c1fc6a86d01029075f4b9ff85ea9f3cc1c63a7ad40b4b30f9b46@ec2-54-235-249-33.compute-1.amazonaws.com:5432/dbkd3lbq4r5ij8"
 
 # connect ActiveRecord with the current database
 ActiveRecord::Base.establish_connection(
